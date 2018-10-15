cec0b46c21beb49e65198f8c9a0d71f8db7e27e1
Ethan <s4911672@bournemouth.ac.uk>
2018-10-15 21:30:55 +0100

    quick commit in case swapping to a new version of unity fucks everything

---
+++ IND Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Camera camera;
+    public NavMeshAgent agent;
+    RaycastHit hit;
+   
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log("Running");
+        if (Input.GetMouseButtonDown(0))
+
+        {
+
+            Debug.Log("Mouse Clicked");
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+
+                agent.SetDestination(hit.point);
+               
+
+
+            }
+
+        }
+
+        
+
+    }
+
+
+}
+
+
+
