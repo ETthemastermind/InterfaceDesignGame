e0f2172f5379500f07fc8b2d36b0069a2aa005f8
Ethan <s4911672@bournemouth.ac.uk>
2018-10-21 20:13:59 +0100

    added more low poly assets from the assets store, gone through all current labs and added the code to make the game work

---
+++ IND Game/Assets/Collect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collect : MonoBehaviour {
+
+    private AudioSource Source;
+    [SerializeField] private AudioClip Clip;
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+
+            Source = other.GetComponent<AudioSource>();
+            Source.PlayOneShot(Clip, 1.0f);
+
+
+            Destroy(gameObject);
+        }
+
+
+        
+    }
+}
--- IND Game/Assets/Scripts/PlayerController.cs
+++ IND Game/Assets/Scripts/PlayerController.cs
@@ -9,12 +9,17 @@ public class PlayerController : MonoBehaviour
     public Camera camera;
     public NavMeshAgent agent;
     RaycastHit hit;
+    private Animator myAnim;
+    private float dist;
+    Quaternion newRotation;
+    float rotSpeed = 5f;
+   
 
 
     // Use this for initialization
     void Start()
     {
-        
+        myAnim = GetComponent<Animator>();
 
     }
 
@@ -23,19 +28,37 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("mOUSE clicked");
+            Debug.Log("MOUSE clicked");
             Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+
+
 
             if (Physics.Raycast(ray, out hit))
             {
+                Debug.Log("got target");
 
                 agent.SetDestination(hit.point);
+                myAnim.SetBool("IsRunning", true);
                
 
             }
-
         }
 
+        dist = Vector3.Distance(hit.point, transform.position);
+        
+        if (dist < 1f)
+        {
+            myAnim.SetBool("IsRunning", false);
+        }
+
+
+        Vector3 relativePos = hit.point - transform.position;
+        newRotation = Quaternion.LookRotation(relativePos, Vector3.up);
+        newRotation.x = 0.0f;
+        newRotation.z = 0.0f;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotSpeed * Time.deltaTime);
         
 
     }
